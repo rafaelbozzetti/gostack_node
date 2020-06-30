2083033e633bf4f2242b4a8ed7d8f8e59a436840
Rafael Pereira Bozzetti <rafaelbozzetti@gmail.com>
2020-06-30 14:30:34 -0300

    task: file upload method

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
+
 
 usersRouter.post('/', async (request, response) => {
 
   try {
 
     const { name, email, password } = request.body;
-
     const createUser = new CreateUserService();
-
     const user = await createUser.execute({
       name,
       email,
@@ -23,9 +27,16 @@ usersRouter.post('/', async (request, response) => {
 
   } catch(err) {
     return response.status(400).json({error: err.message })
-
   }
 
 });
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+
+  console.log(request.file);
+
+  return response.json({ ok: true});
+})
+
 
 export default usersRouter;
