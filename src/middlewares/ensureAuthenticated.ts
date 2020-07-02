c799f0e25a3ebda9e5c3ad7497f10cbbab781ac4
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-02 14:15:12 -0300

    task: Avatar\'s migrations, route, service and model

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -10,7 +10,7 @@ interface TokenPayload {
 
 export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
 
-  const authHeader = request.headers.authorizarion;
+  const authHeader = request.headers.authorization;
 
   if(! authHeader ) {
     throw new Error('JWT token is missing');
@@ -20,9 +20,9 @@ export default function ensureAuthenticated(request: Request, response: Response
   const [, token] = authHeader.split(' ');
 
   try {
-    
+
     const decoded = verify(token, authConfig.jwt.secret);
-    
+
     const { sub } = decoded as TokenPayload;
 
     request.user = {
